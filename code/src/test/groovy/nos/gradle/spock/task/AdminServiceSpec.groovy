fa9393f5b3bb96febbc507d92c78bbf9183dc055
U-NOS\espen <espen@ES-NY.nos.no>
2012-01-24 15:13:43 +0100

    TK og ES

--- code/src/test/groovy/nos/gradle/spock/task/AdminServiceSpec.groovy
+++ code/src/test/groovy/nos/gradle/spock/task/AdminServiceSpec.groovy
@@ -2,6 +2,7 @@ package nos.gradle.spock.task;
 
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * Simple test of {@link AdminService}
@@ -9,12 +10,62 @@ import spock.lang.Specification
  * @author Stig Kleppe-Jorgensen, 2012.01.24
  */
 class AdminServiceSpec extends Specification {
+	
+	AdminService service
+	
+	Repository repo 
+
+	def setup() {
+		repo = Mock()
+		service = new AdminService(repo)
+	}
+	
+	
     def 'mocking and stubbing repository'() {
+		given: 'mock of load'
+		repo.load(_) >> { msgs ->
+			msgs[0] == "user" ? ["lasse"] as SortedSet : null
+		}
+		
+		when:
+		service.addUser("lasse")
+		
+		then:
+		1 * repo.persist("user", "lasse")
+		
+		when:
+		Set<String> users = service.listUsers()
+		
+		then:
+		users == ["lasse"] as Set
     }
 
     def 'check for exception thrown'() {
+		given: 'mock of load'
+		repo.load(_) >> { 
+			throw new IllegalArgumentException("Yepp!")
+		}
+		
+		when:
+		service.listUsers()
+		
+		then:
+		IllegalArgumentException e2 = thrown()
+		e2.getMessage() == "Yepp!"
     }
 
+	@Unroll({"$name is $result"})
     def 'validation with several entries and @Unroll'() {
+		when:
+		def valid = service.validate(name)
+		
+		then:
+		valid == result
+		
+		where:
+		name | result
+		"lasselur" | false
+		"sigrid" | true
+		"true" | false
     }
 }
